a7c0da85fc25aa366e081480100827b7abcb5ad3
SamuelSteward <samuelsteward2003@gmail.com>
2025-10-22 23:30:18 +0530

    Updated Images path

--- src/components/Search.jsx
+++ src/components/Search.jsx
@@ -4,7 +4,7 @@ const Search = ({searchTerm , setSearchTerm}) => {
   return (
     <div className='search'>
         <div>
-            <img src="/search.svg" alt="" />
+            <img src="search.svg" alt="" />
             
             <input type="text" 
                 placeholder='Search through million movies'
